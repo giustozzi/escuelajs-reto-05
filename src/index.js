95d5b956ee9b69dd4bc7e72100a7a5cc8d4ea87d
Mauro Giustozzi <mauro@giustozzi.design>
2019-10-07 16:38:07 -0500

    Solucion 1, 2 y 3

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://rickandmortyapi.com/api/character/';
+const API2 = 'https://us-central1-escuelajs-api.cloudfunctions.net/characters';
 
-const getData = api => {
-  fetch(api)
+var urlNext = '';
+const myStorage = window.localStorage;
+
+
+const getData = api => { 
+  if(!api) {
+    return 'lastPage'
+  }
+  let rr = fetch(api)
     .then(response => response.json())
     .then(response => {
       const characters = response.results;
+      const info = response.info;
+
       let output = characters.map(character => {
         return `
       <article class="Card">
@@ -15,16 +25,39 @@ const getData = api => {
       </article>
     `
       }).join('');
+
+      const nextPage = info.next;
+
+      myStorage.setItem('next_fetch', nextPage)
+
       let newItem = document.createElement('section');
       newItem.classList.add('Items');
       newItem.innerHTML = output;
       $app.appendChild(newItem);
+       return nextPage;
+
     })
-    .catch(error => console.log(error));
+    .catch(error => error );
+
+      return rr;
 }
 
-const loadData = () => {
-  getData(API);
+
+const loadData = async () => {
+  const check = myStorage.getItem('next_fetch')
+  console.log('chequeando',check)    
+
+  urlNext = (check)?check:API; //forma abreviada de hacer lo que esta abajo comentado
+  // if(check) {
+  //   urlNext = check;  // let no const porque se va a estar modiifcado
+  // } else {
+  //   urlNext = API2;
+  // }
+  
+  const result = await getData(urlNext);
+    if(result == 'lastPage') {
+      alert('Ya no hay personajes...')
+    } // = asignacion == comparacion
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
@@ -36,3 +69,7 @@ const intersectionObserver = new IntersectionObserver(entries => {
 });
 
 intersectionObserver.observe($observe);
+
+window.onbeforeunload = function() {
+  myStorage.removeItem('next_fetch');
+}
